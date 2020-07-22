95f185de91ee5ffb8df59f4f2e7b40b036c80f7a
rubinshreinc <rc@sem-wolf.org>
2020-07-22 13:39:31 +0300

    fix bugs in read_data function

--- auto_complete.py
+++ auto_complete.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-
-data = defaultdict()
+import json
+data = defaultdict(set)
+list_data_sentences = []
 
 class AutoCompleteData:
     def __init__(self, completed_sentence, source_text, offset, score):
@@ -13,7 +14,7 @@ class AutoCompleteData:
         return self.score
     
     def get_completed_sentence(self):
-        return self.score
+        return self.completed_sentence
 
     def set_score(self, sub_score):
         self.score -= sub_score
@@ -28,59 +29,64 @@ class AutoCompleteData:
 
 def read_data(filename):
    
-   data_file = open(filename)
-   data_sentences = data_file.read().split("\n")
+    data_file = open(filename)
+    data_sentences = data_file.read().split("\n")
+    offset = 0
+    for sentence in data_sentences:
+    
+        if sentence:
+            first_word = sentence.split()[0]         
+            first_word = sentence.split()[0]         
+            first_word = sentence.split()[0]         
 
-   for sentence in data_sentences:
-   
-      if sentence:
-         first_word = sentence.split()[0]         
-
-         for i in range(len(first_word)):
-            for j in range(i + 1, len(first_word) + 1):        
-                new_ = AutoCompleteData(sentence, filename, 0, 2*len(first_word[i: j]))
-                data[first_word[i: j]].add(new_)
+            for i in range(len(first_word)):     
+                for j in range(i + 1, len(first_word) + 1):        
+                    new_ = AutoCompleteData(sentence, filename, offset, 2*len(first_word[i: j]))
+                    data[first_word[i: j]].add(offset) 
+        list_data_sentences.append(new_)
+        offset += 1
       
 
-def change_prefix(prefix):
-    changed_prefix = []
+#   changed_prefix = []
 
-    for i in range(len(prefix)):
-        for ch in range(ord('a'), ord('z')+1):
-            string_to_search = data.get(prefix.replace(prefix[i], ch, 1))
-            if str:
-                string_to_search.set_score(2+(1 if i > 3 else 5 - i))
-                changed_prefix.append(string_to_search)
-    return changed_prefix
+#     for i in range(len(prefix)):
+#         for ch in range(ord('a'), ord('z')+1):
+#             string_to_search = data.get(prefix.replace(prefix[i], ch, 1))
+#             if str:
+#                 strdef change_prefix(prefix):
+#   ing_to_search.set_score(2+(1 if i > 3 else 5 - i))
+#                 changed_prefix.append(string_to_search)
+#     return changed_prefix
 
-def erase_prefix(prefix):
-    erased_prefix = []
+# def erase_prefix(prefix):
+#     erased_prefix = []
 
-    for i in range(len(prefix)):
-        string_to_search = data.get(prefix.replace(prefix[i], "", 1))
-        if string_to_search:
-            string_to_search.set_score(2+(2 if i > 3 else 10 - 2*i)),
-            erased_prefix.append(string_to_search)
-    return erased_prefix
+#     for i in range(len(prefix)):
+#         strings_to_search = [list_data_sentences[str] for str in data.get(prefix.replace(prefix[i], "", 1))]
+#         if string_to_search:
+#             for str in strings_to_search:
+#                 string_to_search.set_score(2+(2 if i > 3 else 10 - 2*i)),
+#                 erased_prefix.append(string_to_search)
+#     return erased_prefix
 
-def add_prefix(prefix):
-    added_prefix = []
+# def add_prefix(prefix):
+#     added_prefix = []
 
-    for i in range(len(prefix)):
-        for ch in range(ord('a'), ord('z')+1):
-            string_to_search = data.get(prefix[:i+1] + ch + prefix[i+1:])
-            if str:
-                string_to_search.set_score(2+(1 if i > 3 else 5 - i))
-                added_prefix.append(string_to_search)
-    return add_prefix
+#     for i in range(len(prefix)):
+#         for ch in range(ord('a'), ord('z')+1):
+#             string_to_search = data.get(prefix[:i+1] + ch + prefix[i+1:])
+#             if str:
+#                 string_to_search.set_score(2+(1 if i > 3 else 5 - i))
+#                 added_prefix.append(string_to_search)
+#     return add_prefix
 
 
-def complete_prefix(prefix):
-    
-    completed_prefix = change_prefix(prefix)
-    completed_prefix += erase_prefix(prefix)
-    completed_prefix += add_prefix(prefix)
-    return completed_prefix
+# def complete_prefix(prefix):
+#     completed_prefix = []
+#     # completed_prefix = change_prefix(prefix)
+#     completed_prefix += erase_prefix(prefix)
+#     # completed_prefix += add_prefix(prefix)
+#     return completed_prefix
 
 
 
@@ -89,36 +95,44 @@ def get_best_k_completions(prefix):
 
     k = 5
     found_completions = []
-    found_completions = list(data.get(prefix))
-    
+    found_completions = [list_data_sentences[item] for item in data.get(prefix)]
 
     if(len(found_completions) < k):
         found_completions += complete_prefix(prefix)
 
-    found_completions = sorted(found_completions,  key=lambda x: x.get_score())
+    found_completions = sorted(found_completions, key=lambda x: (x.get_score(), x.get_completed_sentence()))
     return found_completions[:k]
 
 
-
-def menu():
-    print("Loading the file and prepraring the system...")
-    print("The system is ready. Enter your text:")
+# def menu():
+#     print("Loading the file and prepraring the system...")
+#     print("The system is ready. Enter your text:")
     
-    while(1):
-        substring = input()
-        suggestions = get_best_k_completions(substring)
-        if suggestions:
-            print("Here are 5 suggestions:")
-            for suggest in suggestions:
-                i = 0
-                print(f"{i}. {suggest.get_completed_sentence()} ({suggest.get_offset()} {suggest.get_completed_sentence()})")
+#     while(1):
+#         substring = input()
+#         suggestions = get_best_k_completions(substring)
+#         if suggestions:
+#             print("Here are 5 suggestions:")
+#             for suggest in suggestions:
+#                 i = 0
+#                 print(f"{i}. {suggest.get_completed_sentence()} ({suggest.get_offset()} {suggest.get_completed_sentence()})")
 
-        else:
-            print("There are no suggestions")
+#         else:
+#             print("There are no suggestions")
 
 
 
 
 read_data("about.txt")
-menu()
+# # menu()
 
+# # for item in data:
+# #     print(data[item])
+
+# for item in sentences:
+#     print(item.get_completed_sentence())
+
+
+b = get_best_k_completions("ou")
+for complete in b:
+    print(f"sentence: {complete.get_completed_sentence()} offset: {complete.get_offset()} score:  {complete.get_score()}")
