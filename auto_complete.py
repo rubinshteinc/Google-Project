1cbfa6da1675cd03447bb39040de1d66e7f95979
rubinshreinc <rc@sem-wolf.org>
2020-07-23 14:06:34 +0300

    add AutoComplete class saving the suggestion to complete

--- auto_complete.py
+++ auto_complete.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from copy import deepcopy
+import time
 
 data = defaultdict(set)
 list_data_sentences = []
@@ -26,6 +27,73 @@ class AutoCompleteData:
     def get_offset(self):
         return self.offset
 
+
+class AutoComplete:
+    def __init__(self, prefix):
+        self.found_completions = {}
+        self.prefix = prefix
+        self.len_prefix = len(prefix)
+
+    def get_found_completion(self):
+        return self.found_completions
+
+    def update_founded_completions(self, obj):
+        search_obj = self.found_completions.get(obj.get_completed_sentence())
+        if search_obj:
+            if obj.get_score() > search_obj.get_score():
+                search_obj.set_score(obj.get_score())
+        else:
+            self.found_completions[obj.get_completed_sentence()] = obj
+
+    def sort(self):
+        {k: v for k, v in sorted(self.found_completions.items(), key=lambda item: item[1].get_score(), reverse=True)}
+
+
+    def change_prefix(self):
+        for i in range(self.len_prefix):
+            for ch in range(ord('a'), ord('z')+1):
+                if chr(ch) not in self.prefix:
+                    strings_to_search = data.get(self.prefix.replace(self.prefix[i], chr(ch), 1))
+
+                    if strings_to_search:
+                        for str in strings_to_search:
+                            changed = deepcopy(list_data_sentences[str])
+                            changed.set_score(2*(self.len_prefix -1) - (1 if i > 3 else 5 - i))
+                            self.update_founded_completions(changed)
+
+    def add_prefix(self):
+        for i in range(self.len_prefix):
+            strings_to_search = data.get(self.prefix.replace(self.prefix[i], "", 1))
+            
+            if strings_to_search:
+                for str in strings_to_search:
+                    added = deepcopy(list_data_sentences[str])
+                    added.set_score(2*(self.len_prefix -1) - (2 if i > 3 else 10 - 2*i))
+                    self.update_founded_completions(added)
+
+
+
+    def erase_prefix(self):
+        for i in range(self.len_prefix):
+            for ch in range(ord('a'), ord('z')+1):
+                if chr(ch) not in self.prefix:
+                    strings_to_search = data.get(self.prefix[:i] + chr(ch) + self.prefix[i:])
+                    
+                    if strings_to_search:
+                        for str in strings_to_search:
+                            erased = deepcopy(list_data_sentences[str])
+                            erased.set_score(2*(self.len_prefix) - (2 if i > 3 else 10 - 2*i))
+                            self.update_founded_completions(erased)
+
+
+    def complete_prefix(self):
+        self.change_prefix()
+        self.erase_prefix()
+        self.add_prefix()
+        self.sort()
+
+
+
 def prefix_ignore_casing(prefix):
     prefix = " ".join(prefix.split())
     return "".join(filter(lambda x: x.isalnum() or x.isspace(), prefix)).lower()
@@ -47,88 +115,31 @@ def initilized_data(filename):
         offset += 1
 
 
-def change_prefix(prefix):
-    changed_prefix = set()
-
-    for i in range(len(prefix)):
-        for ch in range(ord('a'), ord('z')+1):
-            if chr(ch) not in prefix:
-                strings_to_search = data.get(prefix.replace(prefix[i], chr(ch), 1))
-
-                if strings_to_search:
-                    for str in strings_to_search:
-                        changed = deepcopy(list_data_sentences[str])
-                        changed.set_score(2*(len(prefix) -1) - (1 if i > 3 else 5 - i))
-                        changed_prefix.add(changed)
-    return changed_prefix
-
-
-def add_prefix(prefix):
-    added_prefix = set()
-
-    for i in range(len(prefix)):
-        strings_to_search = data.get(prefix.replace(prefix[i], "", 1))
-        
-        if strings_to_search:
-            for str in strings_to_search:
-                added = deepcopy(list_data_sentences[str])
-                added.set_score(2*(len(prefix) -1) - (2 if i > 3 else 10 - 2*i))
-                added_prefix.add(added)
-    return added_prefix
-
-
-
-def erase_prefix(prefix):
-    erased_prefix = set()
-
-    for i in range(len(prefix)):
-        for ch in range(ord('a'), ord('z')+1):
-            if chr(ch) not in prefix:
-                strings_to_search = data.get(prefix[:i+1] + chr(ch) + prefix[i:])
-                
-                if strings_to_search:
-                    for str in strings_to_search:
-                        erased = deepcopy(list_data_sentences[str])
-                        erased.set_score(2*(len(prefix)) - (2 if i > 3 else 10 - 2*i))
-                        erased_prefix.add(erased)
-    return erased_prefix
-
-
-def complete_prefix(prefix, num_of_substring):
-    completed_prefix = set()
-    completed_prefix.update(change_prefix(prefix))
-    completed_prefix.update(erase_prefix(prefix))
-    completed_prefix.update(add_prefix(prefix))
-    return completed_prefix
-
-
-
-
 def get_best_k_completions(prefix):
 
     k = 5
-    found_completions = set()
+    completion = AutoComplete(prefix)
     if (data.get(prefix)):
-        # found_completions.add(list_data_sentences[item].set_score(len(prefix)) for item in data.get(prefix))
         for item in data.get(prefix):
             complete = list_data_sentences[item]  
             complete.set_score(2 * len(prefix)) 
-            found_completions.add(complete)
+            completion.update_founded_completions(complete)
 
-    if len(found_completions) < k:
-        found_completions.update(complete_prefix(prefix, k - len(found_completions)))
+    if len(completion.get_found_completion()) < k:
+        completion.complete_prefix()
+    completion_list = [value for value in completion.get_found_completion().values()]
 
-    found_completions = sorted(found_completions, key=lambda x: (x.get_score(), x.get_completed_sentence()), reverse=True)
-    return found_completions[:k]
-
+    return completion_list[:k]
 
 def menu():
 
     print("Loading the file and prepraring the system...")
     initilized_data("about.txt")
     print("The system is ready. Enter your text:")
-    
+
     prefix = input()
+    start_time = time.time()
+
     while(True):
         if("#" == prefix[-1]):
             prefix = input()
@@ -141,18 +152,20 @@ def menu():
                 i += 1
                 print(f"{i}. {suggest.get_completed_sentence()} ({suggest.get_offset()} {suggest.get_source_text()} {suggest.get_score()})")
             print(prefix, end="")
+            print("--- %s seconds ---" % (time.time() - start_time))
+
         else:
             print("There are no suggestions")
         prefix += input()
-
-
-def process():
-    menu()
+        start_time = time.time()
 
 
 
+menu()
 
-process()
+
+
+
 
 
 
